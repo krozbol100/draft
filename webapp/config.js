46657023631c353cada22a68cf7e18c9aba14320
Sadmir Duric <sadmir.duric@comtrade.com>
2021-01-11 00:28:11 +0100

    setparameterrs test

---
+++ webapp/config.js
@@ -0,0 +1,6 @@
+window['settings'] = {
+    authBaseUrl: 'https://@@replaceme@@.azurewebsites.net/api/user/',
+    searchService: 'https://@@replaceme@@.azurewebsites.net/api/user/ActiveDirectorySearch',
+    webApiUrl: 'https://@@replaceme@@.azurewebsites.net/',
+    signarRUrl: 'https://@@replaceme@@.azurewebsites.net/signalr'
+}
---
+++ webapp/config_orig.js
@@ -0,0 +1,6 @@
+window['settings'] = {
+    authBaseUrl: 'https://esessions-core.azurewebsites.net/api/user/',
+    searchService: 'https://esessions-core.azurewebsites.net/api/user/ActiveDirectorySearch',
+    webApiUrl: 'https://esessions-core.azurewebsites.net/',
+    signarRUrl: 'https://esessions-core.azurewebsites.net/signalr'
+}
