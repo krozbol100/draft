ba5ec3c8b6c96f0ad872e3f50ba07f9912d851e1
Sadmir Duric <sadmir.duric@comtrade.com>
2021-01-11 01:18:30 +0100

    managed app: with front app setparameters in place finally.

--- webapp/config.js
+++
@@ -1,6 +0,0 @@
-window['settings'] = {
-    authBaseUrl: 'https://@@replaceme@@.azurewebsites.net/api/user/',
-    searchService: 'https://@@replaceme@@.azurewebsites.net/api/user/ActiveDirectorySearch',
-    webApiUrl: 'https://@@replaceme@@.azurewebsites.net/',
-    signarRUrl: 'https://@@replaceme@@.azurewebsites.net/signalr'
-}
--- webapp/config_orig.js
+++
@@ -1,6 +0,0 @@
-window['settings'] = {
-    authBaseUrl: 'https://esessions-core.azurewebsites.net/api/user/',
-    searchService: 'https://esessions-core.azurewebsites.net/api/user/ActiveDirectorySearch',
-    webApiUrl: 'https://esessions-core.azurewebsites.net/',
-    signarRUrl: 'https://esessions-core.azurewebsites.net/signalr'
-}
